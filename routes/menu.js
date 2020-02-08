b08b85ac0ee33cb4c79b749c0377ad188e0af04a
Wilson Wong <wilwong89@gmail.com>
2020-02-08 23:02:56 +0000

    Dummy data setup and connected

--- routes/menu.js
+++ routes/menu.js
@@ -1,20 +1,22 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const dummyMenu = require("../src/dummyData/menuItems");
 
-router.get('/', (req, res) => {
-  res.send('You got /menu');
+router.get("/", (req, res) => {
+  console.log("dummyMenu", dummyMenu);
+  res.send(dummyMenu);
 });
 
-router.post('/', (req, res) => {
-  res.send('You posted to /menu');
+router.post("/", (req, res) => {
+  res.send("You posted to /menu");
 });
 
-router.put('/:id', (req, res) => {
-  res.send('You are updating a menu item');
+router.put("/:id", (req, res) => {
+  res.send("You are updating a menu item");
 });
 
-router.delete('/:id', (req, res) => {
-  res.send('You are deleting a menu item');
+router.delete("/:id", (req, res) => {
+  res.send("You are deleting a menu item");
 });
 
 module.exports = router;
---
+++ src/dummyData/menuItems.js
@@ -0,0 +1,46 @@
+/*
+let menuRecord = {
+  id: "",
+  name: "",
+  desc: "",
+  price: "",
+  img_url: "",
+  deleted_at: ""
+}
+*/
+let dummyMenuDB = [
+  {
+    id: "01",
+    name: "Pizza",
+    desc: "Peperoni, cheese, tomato sauce.",
+    price: 9.99,
+    img_url: "https://i.imgur.com/YPacqGc.jpg",
+    deleted_at: null
+  },
+  {
+    id: "02",
+    name: "Fries",
+    desc: "Potaters",
+    price: 5.55,
+    img_url: "https://i.imgur.com/HTtLQol.jpg",
+    deleted_at: null
+  },
+  {
+    id: "03",
+    name: "Pasta",
+    desc: "Pasta, of some sort? ¯\\_(ツ)_/¯",
+    price: 12.99,
+    img_url: "https://i.imgur.com/HMVBN9l.jpg",
+    deleted_at: null
+  },
+  {
+    id: "04",
+    name: "Ramen",
+    desc: "Instant noodles, some broth. Questionable protein.",
+    price: 0.99,
+    img_url: "https://i.imgur.com/EIZGrN2.jpg",
+    deleted_at: null
+  }
+];
+
+module.exports = dummyMenuDB;
