21c308a04a820d190824e3f41ed3b608da5a2b71
Bryce McMath <bryce.j.mcmath@gmail.com>
2020-02-08 21:36:41 -0800

    Reformat SQL setup

--- queries/seed/seed.sql
+++ queries/seed/seed.sql
@@ -1,41 +1,41 @@
 INSERT INTO "User" (
-  name,
-  email,
-  role,
-  mobile,
-  password) 
+  "name",
+  "email",
+  "role",
+  "mobile",
+  "password") 
 VALUES 
-('Wilson Wong', "wilwong89@gmail.com", 'customer', '250 896 8729', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
-('Bryce McMath', "bryce.j.mcmath@gmail.com", 'operator', '250 896 8729', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+('Wilson Wong', 'wilwong89@gmail.com', 'customer', '250 896 8729', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Bryce McMath', 'bryce.j.mcmath@gmail.com', 'operator', '250 896 8729', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
 
 INSERT INTO "Order" (
-  pickup_name,
-  customer_note,
-  created_at,
-  estimate,
-  total_price,
-  accepted_at,
-  deleted_at,
-  user_id) 
+  "pickup_name",
+  "customer_note",
+  "created_at",
+  "estimate",
+  "total_price",
+  "accepted_at",
+  "deleted_at",
+  "user_id") 
 VALUES 
-('Wilson Wong', "I don't like pickles", now(), NULL, 1989, NULL, NULL, 1);
+('Wilson Wong', 'I do not like pickles', now(), NULL, 1989, NULL, NULL, 1);
 
 INSERT INTO "MenuItem" (
-  name,
+  "name",
   "desc",
-  price,
-  img_url,
-  deleted_at) 
+  "price",
+  "img_url",
+  "deleted_at") 
 VALUES 
-('Pizza', "Pepperoni, cheese, tomato sauce, just like a mama used ta make", 999, "https://i.imgur.com/YPacqGc.jpg", NULL),
-("Fries", "French fried purtaters", 599, "https://i.imgur.com/HTtLQol.jpg", NULL),
-("Pasta", "Ah spaghett", 999, "https://i.imgur.com/HMVBN9l.jpg", NULL);
+('Pizza', 'Pepperoni, cheese, tomato sauce, just like a mama used ta make', 999, 'https://i.imgur.com/YPacqGc.jpg', NULL),
+('Fries', 'French fried purtaters', 599, 'https://i.imgur.com/HTtLQol.jpg', NULL),
+('Pasta', 'Ah spaghett', 999, 'https://i.imgur.com/HMVBN9l.jpg', NULL);
 
 INSERT INTO "OrderItem" (
-  order_id,
-  menu_item_id
+  "order_id",
+  "menu_item_id"
 ) 
-VALUE
+VALUES
 (1, 1),
 (1, 1),
 (1, 2),
