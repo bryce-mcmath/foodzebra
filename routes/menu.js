b8038b37bae59906a337dbbdd95dc013379c17fe
Wilson Wong <wilwong89@gmail.com>
2020-02-08 16:24:22 -0800

    /menu/:id route can display data to front end

--- routes/menu.js
+++ routes/menu.js
@@ -2,13 +2,33 @@ const express = require("express");
 const router = express.Router();
 const dummyMenu = require("../src/dummyData/menuItems");
 
+// Routes: /
+
 router.get("/", (req, res) => {
-  console.log("dummyMenu", dummyMenu);
   res.send(dummyMenu);
 });
 
 router.post("/", (req, res) => {
   res.send("You posted to /menu");
+});
+
+// Routes: /menu/:id params
+
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  let returnData = null;
+
+  if (typeof id === "string") {
+    for (let i = 0; i < dummyMenu.length; i++) {
+      if (dummyMenu[i].id === id) {
+        returnData = dummyMenu[i];
+      }
+    }
+
+    res.send(returnData);
+  } else {
+    res.status(400);
+  }
 });
 
 router.put("/:id", (req, res) => {
