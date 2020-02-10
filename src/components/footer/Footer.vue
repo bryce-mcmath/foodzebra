d987debd09447fdfdfd2a7175266641b16694b24
Bryce McMath <bryce.j.mcmath@gmail.com>
2020-02-10 10:43:47 -0800

    Add footer content

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -5,7 +5,24 @@
       <div class="brand"><span>food</span>zebra</div>
       <hr />
     </div>
-    <div class="footer-content grid"></div>
+    <section class="footer-content grid">
+      <div class="location-contact flex">
+        <h3 class="footer-header">Location and Contact</h3>
+        <p>314 Delphi Street</p>
+        <p>Victoria, BC V9A 4E6</p>
+        <p>Contact Us | 888.888.8888</p>
+      </div>
+      <div class="mailing flex">
+        <h3 class="footer-header">Join Our Mailing List</h3>
+        <form action="#" class="flex">
+          <input type="text" placeholder="Email Address" />
+          <input type="submit" value="Sign Up" class="ui button" />
+        </form>
+      </div>
+    </section>
+    <div class="copyright">
+      &copy;2020 | Privacy Policy | Github
+    </div>
   </footer>
 </template>
 
