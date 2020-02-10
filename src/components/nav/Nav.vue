c0d255214e76278e8984f96ecc332db38091d3c8
Wilson Wong <wilwong89@gmail.com>
2020-02-10 15:21:48 -0800

    fix Nav.vue template type

--- src/components/nav/Nav.vue
+++ src/components/nav/Nav.vue
@@ -3,7 +3,7 @@
     <nav class="nav flex" v-bind:class="{ transparent: !isScrolled }">
       <ul class="nav-left flex">
         <li>
-          <a shref="#menu">Menu</a>
+          <a href="#menu">Menu</a>
         </li>
         <li>
           <a href="#">Events</a>
