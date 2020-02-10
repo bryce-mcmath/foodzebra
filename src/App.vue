9fb74869db3e7cc8240bc07b1a76be1c315cee7c
Bryce McMath <bryce.j.mcmath@gmail.com>
2020-02-09 22:24:34 -0800

    Add menuitems placeholder

--- src/App.vue
+++ src/App.vue
@@ -3,30 +3,33 @@
     <Nav></Nav>
     <!-- if on / -->
     <Hero></Hero>
-    <!-- <Menu /> -->
+    <MenuItems></MenuItems>
     <!-- if on /orders as operator -->
     <!-- <Orders /> -->
-    <!-- <Footer />  -->
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
-import Nav from "./components/nav/Nav.vue";
-import Hero from "./components/hero/Hero.vue";
-
+import Nav from './components/nav/Nav.vue';
+import Hero from './components/hero/Hero.vue';
+import Footer from './components/footer/Footer.vue';
+import MenuItems from './components/menu_items/MenuItems.vue';
 export default {
-  name: "app",
+  name: 'app',
   components: {
     Nav,
-    Hero
+    Hero,
+    MenuItems,
+    Footer
   },
   data() {
     return {
       cart: {
         items: [],
-        mobile: "",
-        customer_note: "",
-        pickup_name: ""
+        mobile: '',
+        customer_note: '',
+        pickup_name: ''
       }
     };
   }
@@ -34,5 +37,5 @@ export default {
 </script>
 
 <style lang="scss">
-@import "./app.scss";
+@import './app.scss';
 </style>
---
+++ src/components/footer/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <footer class="footer">
+    <div class="footer-brand flex">
+      <hr />
+      <div class="brand"><span>food</span>zebra</div>
+      <hr />
+    </div>
+    <div class="footer-content grid"></div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  data: function() {
+    return {};
+  }
+};
+</script>
