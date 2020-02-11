44668bc5971670bb1e54fe2744f3bd32f16b3c04
Wilson Wong <wilwong89@gmail.com>
2020-02-10 16:10:14 -0800

    some merge

--- src/api/ajaxCalls.js
+++ src/api/ajaxCalls.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 
 module.exports = {
+  testCall: (id = "") => {
+    console.log("ajax:", id);
+    return axios.put(`/menu/${id}`).then(response => response.data);
+  },
   getAllMenuItems: () => {
     return axios.get("/menu").then(response => response.data);
   },
